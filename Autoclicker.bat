e1397329b75fbe142cab5bba52b85cdc87546a7a
Smilos71 <147918915+Smilos71@users.noreply.github.com>
2023-11-12 08:20:09 +0100

    Add files via upload

---
+++ Autoclicker.bat
@@ -0,0 +1,72 @@
+@echo off
+setlocal
+title virus_installer.exe
+timeout /t 0.5
+color 2
+echo Hacking in progress..
+timeout /t 1
+echo                          Hacking in progress..
+timeout /t 1
+echo                                               Hacking in progress.. 
+echo Hacking in progress..
+timeout /t 1
+echo                          Hacking in progress..
+timeout /t 1
+echo                                               Hacking in progress.. 
+echo Hacking in progress..
+timeout /t 1
+echo                          Hacking in progress..
+timeout /t 1
+echo                                               Hacking in progress.. 
+echo Hacking in progress..
+timeout /t 1
+echo                          Hacking in progress..
+timeout /t 1
+echo                                               Hacking in progress.. 
+echo Hacking in progress..
+timeout /t 1
+echo                          Hacking in progress..
+timeout /t 1
+echo                                               Hacking in progress.. 
+echo Hacking in progress..
+timeout /t 1
+echo                          Hacking in progress..
+timeout /t 1
+echo                                               Hacking in progress.. 
+echo Hacking in progress..
+timeout /t 1
+echo                          Hacking in progress..
+timeout /t 1
+echo                                               Hacking in progress.. 
+echo Hacking in progress..
+timeout /t 1
+echo                          Hacking in progress..
+timeout /t 1
+echo                                               Hacking in progress.. 
+timeout /t 2
+color abc
+timeout /t 0.5
+color abc
+timeout /t 0.5
+color abc
+timeout /t 0.5
+color abc
+timeout /t 0.5
+color abc
+timeout /t 0.5
+color abc
+timeout /t 0.5
+color abc
+timeout /t 0.5
+color abc
+timeout /t 0.5
+color abc
+timeout /t 0.5
+color abc
+timeout /t 0.5
+
+color c
+echo =============
+echo   HACKED!
+echo =============
+pause
